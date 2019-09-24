11c0a8b4d30ab4a011c8ac74e994bbdba461ce38
Wafaa Nasr <wafaa.aanasr@gmail.com>
2019-09-24 20:01:55 +0200

    Updates

---
+++ precache-manifest.27977c520acb747e17865f8bf627d6ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e0aad0bbd421414fc4f074becf6ebe",
+    "url": "/monsters-rolodes/index.html"
+  },
+  {
+    "revision": "e1bfa7f2cbe88bca52a2",
+    "url": "/monsters-rolodes/static/css/main.210ada68.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodes/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "e1bfa7f2cbe88bca52a2",
+    "url": "/monsters-rolodes/static/js/main.7897589e.chunk.js"
+  },
+  {
+    "revision": "479ffac6045d37749d7e",
+    "url": "/monsters-rolodes/static/js/runtime~main.43ea932b.js"
+  }
+]);
